7612109c7e11f8ae71e4d3beec99d86b29e6dd2b
huangguorui <2315684336@qq.com>
2019-07-09 09:14:59 +0800

    右侧评论栏目界面修改

--- sidebar.php
+++ sidebar.php
@@ -10,35 +10,43 @@
           <h3>热门评论</h3>
           <div class="comment_list">
             <ul class="clearfix">
-
+            <!-- wordPress原生评论头像获取get_avatar(get_comment_author_email(), 50) -->
+            <!-- 获取QQ昵称和头像（jsonp）http://users.qzone.qq.com/fcg-bin/cgi_get_portrait.fcg?uins=343049466
+             -->
+            <!-- 获取QQ昵称http://q1.qlogo.cn/g?b=qq&nk=843977358&s=40
+                 http://q2.qlogo.cn/headimg_dl?dst_uin=843977358&spec=40 -->
 
             <?php
-            // global $wpdb;
-            // $my_email = get_bloginfo ('admin_email'); // AND comment_author_email != '$my_email' 不展示管理员回复
-            // $sql = "SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url,comment_author_email, SUBSTRING(comment_content,1,100) AS com_excerpt FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID) WHERE comment_approved = '1' AND comment_type = '' AND post_password = '' ORDER BY comment_date_gmt DESC LIMIT 6";
-            // $comments = $wpdb->get_results($sql);
-            // $output = $pre_HTML;
-            // foreach ($comments as $comment) { $com_excerpt = strip_tags($comment->com_excerpt); $excerpt_len = mb_strlen($com_excerpt, 'utf-8');
-            //     if ($excerpt_len > 46) $com_excerpt = mb_substr($com_excerpt, 0, 46, 'utf-8').'...';
-            //     $output .= "\n<li>". '<img src=https://q.qlogo.cn/headimg_dl?bs=qq&dst_uin='.get_comment_author_email().'&src_uin=qq.feixue.me&fid=blog&spec=100&id='.rand(1,1000).'>' .strip_tags($comment->comment_author) . "<span>（" . timeago($comment->comment_date_gmt) . "）</span>" . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
-            // $output .= $post_HTML;
-            // $output = convert_smilies($output);
-            // echo $output;
+            //通过活动qq图像进行设置
+            global $wpdb;
+            $my_email = get_bloginfo ('admin_email'); // AND comment_author_email != '$my_email' 不展示管理员回复
+            $sql = "SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url,comment_author_email, SUBSTRING(comment_content,1,100) AS com_excerpt FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID) WHERE comment_approved = '1' AND comment_type = '' AND post_password = '' ORDER BY comment_date_gmt DESC LIMIT 6";
+            $comments = $wpdb->get_results($sql);
+            $output = $pre_HTML;
+            foreach ($comments as $comment) { $com_excerpt = strip_tags($comment->com_excerpt); $excerpt_len = mb_strlen($com_excerpt, 'utf-8');
+                if ($excerpt_len > 46) $com_excerpt = mb_substr($com_excerpt, 0, 46, 'utf-8').'...';
+                //$output .= "\n<li>". '<img src=https://q.qlogo.cn/headimg_dl?bs=qq&dst_uin='.get_comment_author_email().'&src_uin=qq.feixue.me&fid=blog&spec=100&id='.rand(1,1000).'>' .strip_tags($comment->comment_author) . "<span>（" . timeago($comment->comment_date_gmt) . "）</span>" . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
+                $output .= "\n<li>". '<img src=https://q.qlogo.cn/headimg_dl?bs=qq&dst_uin='.get_comment_author_email().'&src_uin=qq.feixue.me&fid=blog&spec=100&id='.rand(1,1000).'>' .strip_tags($comment->comment_author) . "<span></span>" . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
+
+            $output .= $post_HTML;
+            $output = convert_smilies($output);
+            echo $output;
         ?> 
 
 
               <?php
-              global $wpdb;
-              $my_email = get_bloginfo ('admin_email');
-              $sql = "SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url,comment_author_email, SUBSTRING(comment_content,1,100) AS com_excerpt FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID) WHERE comment_approved = '1' AND comment_type = '' AND post_password = '' AND comment_author_email != '$my_email' ORDER BY comment_date_gmt DESC LIMIT 6";
-              $comments = $wpdb->get_results($sql);
-              $output = $pre_HTML;
-              foreach ($comments as $comment) { $com_excerpt = strip_tags($comment->com_excerpt); $excerpt_len = mb_strlen($com_excerpt, 'utf-8');
-                  if ($excerpt_len > 46) $com_excerpt = mb_substr($com_excerpt, 0, 46, 'utf-8').'...';
-                  $output .= "\n<li>".get_avatar(get_comment_author_email(), 50).strip_tags($comment->comment_author). '：' . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
-              $output .= $post_HTML;
-              $output = convert_smilies($output);
-              echo $output;
+              //下面是wordpress自带的
+              // global $wpdb;
+              // $my_email = get_bloginfo ('admin_email');
+              // $sql = "SELECT DISTINCT ID, post_title, post_password, comment_ID, comment_post_ID, comment_author, comment_date_gmt, comment_approved, comment_type,comment_author_url,comment_author_email, SUBSTRING(comment_content,1,100) AS com_excerpt FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->comments.comment_post_ID = $wpdb->posts.ID) WHERE comment_approved = '1' AND comment_type = '' AND post_password = '' AND comment_author_email != '$my_email' ORDER BY comment_date_gmt DESC LIMIT 6";
+              // $comments = $wpdb->get_results($sql);
+              // $output = $pre_HTML;
+              // foreach ($comments as $comment) { $com_excerpt = strip_tags($comment->com_excerpt); $excerpt_len = mb_strlen($com_excerpt, 'utf-8');
+              //     if ($excerpt_len > 46) $com_excerpt = mb_substr($com_excerpt, 0, 46, 'utf-8').'...';
+              //     $output .= "\n<li>".get_avatar(get_comment_author_email(), 50).strip_tags($comment->comment_author). '：' . "<p>". $com_excerpt ."</p>" . "<a href=\"" . get_permalink($comment->ID) ."#comment-" . $comment->comment_ID . "\" title=\"查看 " .$comment->post_title . "\">评：".$comment->post_title ."</a></li>";}
+              // $output .= $post_HTML;
+              // $output = convert_smilies($output);
+              // echo $output;
           ?> 
             </ul>
           </div>
